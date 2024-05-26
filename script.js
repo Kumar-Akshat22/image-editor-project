c87e5f327bfcf90a8849fbcc263386b3fe7a85e0
Kumar Akshat <kakshat247@gmail.com>
2024-05-26 16:16:32 +0530

    Implemented all the functionalities

--- script.js
+++ script.js
@@ -5,14 +5,35 @@ const rotateImage = document.getElementById("rotateImage");
 const flipImageHorizontal = document.getElementById("flipImageHorizontal");
 const flipImageVertical = document.getElementById("flipImageVertical");
 const cropBtn = document.getElementById("cropBtn");
-
+const frameButtons = document.querySelectorAll(".frame");
+const image = document.getElementById("uploadedImage");
+const outputImage = document.getElementById("outputImage"); 
+const useThisImageButton = document.getElementById("useThisImageButton");
+const finalFramedImage = document.getElementById("finalFramedImage");
+const imageEditor = document.querySelector(".imageEditor");
+const closeEditor = document.querySelector(".closeEditor");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector('.close-button');
+const finalImageContainer = document.querySelector(".finalImageContainer");
 
 let cropper;
-const image = document.getElementById("uploadedImage");
+let frameType = 'original';
 
 chooseImage.addEventListener("click", () => {
     fileInput.click();
 });
+
+
+closeEditor.addEventListener("click" , ()=>{
+
+    imageEditor.classList.remove("active");
+});
+
+closeModal.addEventListener('click' , ()=>{
+
+    modal.style.display = 'none';
+    
+})
 
 fileInput.addEventListener("change", handleFileSelect);
 
@@ -24,6 +45,7 @@ function handleFileSelect(event) {
         
         reader.onload = (e) => {
 
+            imageEditor.classList.add("active");
             image.style.display = "block";
             image.src = e.target.result;
 
@@ -81,11 +103,45 @@ document.getElementById("flipImage").addEventListener("click", function () {
 
 cropBtn.addEventListener('click' , ()=>{
 
+    imageEditor.classList.remove("active");
+    modal.style.display = 'block';
     if(cropper){
 
         const croppedImage = cropper.getCroppedCanvas();
-        const outputImage = document.getElementById("outputImage");
+        
         outputImage.src = croppedImage.toDataURL();
-        cropper.destroy();
+
     }
+
+
+});
+
+frameButtons.forEach(button=>{
+
+    button.addEventListener('click' , (event) => {
+
+        frameType = event.currentTarget.getAttribute('data-frame');
+        console.log(frameType);
+        applyFrame(frameType);
+    })
+})
+
+function applyFrame(frameType){
+
+    if(!outputImage.src)
+        return;
+
+    outputImage.className = `frame ${frameType}`;
+
+
+}
+
+useThisImageButton.addEventListener('click' , ()=>{
+
+    modal.style.display = 'none';
+    const croppedImage = cropper.getCroppedCanvas();
+    finalFramedImage.src = croppedImage.toDataURL();
+    finalFramedImage.className = `frame ${frameType}`;
+    finalImageContainer.classList.add("active");
+    cropper.destroy();
 })
